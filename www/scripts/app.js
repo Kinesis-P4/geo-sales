facdbba4cd2af52083ed4fd85b5acb2fc098e729
Roberto Moya <beto.ldas@gmail.com>
2014-10-27 00:18:36 -0600

    Added the code to handle side menu along with tabs on index.html

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -5,7 +5,63 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('Geosales', ['ionic', 'config'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "tabs.html"
+    })
+    .state('tabs.inicio', {
+      url: "/inicio",
+      views: {
+        'inicio-tab': {
+          templateUrl: "inicio.html",
+          controller: 'InicioTabCtrl'
+        }
+      }
+    })
+    .state('tabs.clientes', {
+      url: "/clientes",
+      views: {
+        'clientes-tab': {
+          templateUrl: "clientes.html"
+        }
+      }
+    })
+    .state('tabs.pendientes', {
+      url: "/pendientes",
+      views: {
+        'pendientes-tab': {
+          templateUrl: "pendientes.html"
+        }
+      }
+    })
+    .state('tabs.rutas', {
+      url: "/rutas",
+      views: {
+        'rutas-tab': {
+          templateUrl: "rutas.html"
+        }
+      }
+    })
+    .state('tabs.estadisticas', {
+      url: "/estadisticas",
+      views: {
+        'estadisticas-tab': {
+          templateUrl: "estadisticas.html"
+        }
+      }
+    })
+    .state('configuracion', {
+      url: '/configuracion',
+      templateUrl: 'configuracion.html'
+    });
 
+
+   $urlRouterProvider.otherwise("/tab/inicio");
+
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
