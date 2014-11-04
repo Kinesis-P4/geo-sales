2135eef4f63d1857c92083576f9c663386a67399
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2014-11-03 18:13:24 -0600

    Add Credit form page

--- www/scripts/controllers/AgregarCreditoController.js
+++ www/scripts/controllers/AgregarCreditoController.js
@@ -3,6 +3,28 @@
 angular.module('Geosales')
   .controller('AgregarCreditoController', ['$scope', 'ClientesServices', 'CreditosServices','$state','$stateParams', function ContentCtrl($scope, ClientesServices, CreditosServices, $state, $stateParams) {
 
+  	$scope.credit = {};
+  	$scope.lines = [{credit: '', name:'', quantity:'', price:''}];
+  	$scope.deposit = 0;
+
+  	$scope.addLine = function() {
+  		$scope.lines.push({credit: '', name:'', quantity:'', price:''});
+  	};
+
+  	$scope.removeLine = function(line) {
+  		$scope.lines.splice($scope.lines.indexOf(line), 1);
+  	};
+
+  	$scope.getTotal = function() {
+  		var total = 0;
+
+  		for (var i = 0; i < $scope.lines.length; i++) {
+  			total += ($scope.lines[i].quantity * $scope.lines[i].price);
+  		};
+
+  		return total;
+  	};
+
     ClientesServices.get($stateParams.id).success(function(data){
         $scope.cliente=data;
     });
