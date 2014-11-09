ff127cef9e93c96de1d537d54db9efec20a79128
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2014-11-09 04:17:00 -0600

    Feature Create Cliente has been added

--- www/scripts/controllers/AgregarClienteController.js
+++ www/scripts/controllers/AgregarClienteController.js
@@ -3,17 +3,24 @@
 angular.module('Geosales')
   .controller('AgregarClienteController', ['$scope', '$rootScope', 'ClientesServices','$state','$stateParams', function ContentCtrl($scope, $rootScope, ClientesServices, $state, $stateParams) {
 
-	var point = new Parse.GeoPoint({latitude: 0, longitude: 0});
-
-	//{ "id": "p38txDyfsu", "created_at": "2014-11-02T21:27:32Z", "updated_at": "2014-11-04T03:15:04Z", "collectDate": 13, "email": "ljblanco@gmail.com", "lastName": "Blanco", "location": { "__type": "GeoPoint", "latitude": 0, "longitude": 0 }, "name": "Luis", "phone": "34128549", "user": [ "_User", "Yi697lxWcF" ] }
-	var user = $rootScope.loggedUser.relation('_User');
-
-  	$scope.cliente = {name:'', lastName:'', email:'', phone:'', location:point, user:user, collectDate:15};
+	$scope.cliente = {name:'', lastName:'', email:'', phone:'', collectDate:15};
 
   	$scope.addClient = function() {
-  		ClientesServices.create($scope.cliente).success(function(data){
-	    	$state.go('/clientes');
-	    });
+  		var Client = Parse.Object.extend('clients');
+  		var newClient = new Client();
+		var defaultPoint = new Parse.GeoPoint({latitude: 0, longitude: 0});
+
+		newClient.set('name', $scope.cliente.name);
+		newClient.set('lastName', $scope.cliente.lastName);
+		newClient.set('email', $scope.cliente.email);
+		newClient.set('phone', $scope.cliente.phone+"");
+		newClient.set('collectDate', $scope.cliente.collectDate);
+		newClient.set('location', defaultPoint);
+		newClient.set('user', Parse.User._currentUser);
+
+		newClient.save();
+
+		//Redireccionar a la pantalla del cliente recien creado
   	};
 
 }]);
