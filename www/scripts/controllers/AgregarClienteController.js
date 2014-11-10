f51416428795a78b24d1490854826052beeaa7eb
Luis Blanco <ljblanco@gmail.com>
2014-11-10 11:23:19 -0600

    Se agrega redirect al guardar cliente

--- www/scripts/controllers/AgregarClienteController.js
+++ www/scripts/controllers/AgregarClienteController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('Geosales')
-  .controller('AgregarClienteController', ['$scope', '$rootScope', 'ClientesServices','$state','$stateParams', function ContentCtrl($scope, $rootScope, ClientesServices, $state, $stateParams) {
+  .controller('AgregarClienteController', ['$scope', '$rootScope', 'ClientesServices','$state','$stateParams', '$ionicPopup', '$location', function ContentCtrl($scope, $rootScope, ClientesServices, $state, $stateParams, $ionicPopup, $location) {
 
 	$scope.cliente = {name:'', lastName:'', email:'', phone:'', collectDate:15};
 
@@ -17,10 +17,38 @@ angular.module('Geosales')
 		newClient.set('collectDate', $scope.cliente.collectDate);
 		newClient.set('location', defaultPoint);
 		newClient.set('user', Parse.User._currentUser);
+		newClient.save({
+		      success: function(newClient){
+		            
+		            $ionicPopup.show({
+			              template: 'El cliente fue guardado exitosamente.',
+			              title: 'Cliente',              
+			              buttons: [
+			                { 
+			                  text: 'Continuar',
+			                  
+			                  onTap: function(e) { $location.path("tab/cliente/" + newClient.id); } 
+			                }
+			              ]
+			              }).then(function(res) {
+			                console.log('Tapped!', res);
+			              }, function(err) {
+			                console.log('Err:', err);
+			              }, function(msg) {
+			                console.log('message:', msg);
+		              });
+		      }
+		});
 
-		newClient.save();
+		// $ionicPopup.show({
+		//      title: 'Cliente guardado',
+		//      template: 'El cliente fue guardado correctamente.'
+		//    });
+		
+		
+		
 
 		//Redireccionar a la pantalla del cliente recien creado
-  	};
+  	};  	
 
 }]);
