a6707e8bcb8f610cadb4d6cbd956919bd2da47ca
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2014-11-10 23:45:42 -0600

    Creating transactions retriving

--- www/scripts/controllers/ClienteController.js
+++ www/scripts/controllers/ClienteController.js
@@ -1,10 +1,25 @@
 'use strict';
 
-angular.module('Geosales')
-  .controller('ClienteController', ['$scope', 'ClientesServices','$state','$stateParams', function ContentCtrl($scope, ClientesServices, $state, $stateParams) {
+angular.module('Geosales').controller('ClienteController', ['$scope', 'ClientesServices','$state','$stateParams', 
+  	function ContentCtrl($scope, ClientesServices, $state, $stateParams) {
 
-    ClientesServices.get($stateParams.id).success(function(data){
-        $scope.cliente=data;
-    });
+  	$scope.transactions = [];
+
+  	$scope.getTransactions = function() {
+		var Log = Parse.Object.extend('account_log');
+		var query = new Parse.Query(Log);
+		query.descending("createdAt");
+		query.include("client");
+		query.include("debit");
+		query.include("credit");
+		query.find({
+			success: function(logs) {
+				$scope.transactions = logs;
+			},
+			error: function() {
+				console.log('Error getting the transaction logs');
+			}
+		});
+  	};
 
 }]);
