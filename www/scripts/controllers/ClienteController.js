eec82cfb9147ce60f8af25f1f9b7b69a2cf6bf44
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2014-12-12 23:36:47 -0600

    Loading Screen

--- www/scripts/controllers/ClienteController.js
+++ www/scripts/controllers/ClienteController.js
@@ -1,7 +1,15 @@
 'use strict';
 
-angular.module('Geosales').controller('ClienteController', ['$scope', 'ClientesServices', '$ionicPopup', '$state','$stateParams', 'PARSE_CREDENTIALS', 
-	function ($scope, ClientesServices, $ionicPopup, $state, $stateParams, PARSE_CREDENTIALS) {
+angular.module('Geosales').controller('ClienteController', ['$scope', 'ClientesServices', '$ionicPopup', '$state', '$stateParams', '$ionicLoading', 'PARSE_CREDENTIALS', 
+	function ($scope, ClientesServices, $ionicPopup, $state, $stateParams, $ionicLoading, PARSE_CREDENTIALS) {
+
+  $scope.showLoading = function() {
+    $ionicLoading.show({template: 'Cargando...'});
+  };
+
+  $scope.hideLoading = function() {
+    $ionicLoading.hide();
+  };
 
   var Client = Parse.Object.extend('clients');
   var Debit = Parse.Object.extend('debits');
@@ -17,6 +25,7 @@ angular.module('Geosales').controller('ClienteController', ['$scope', 'ClientesS
   $scope.clienteLoaded = {};
 
   $scope.$on('$viewContentLoaded', function(){
+    $scope.showLoading();
     getCurrentPosition();
     $scope.getClient();
   });
@@ -88,7 +97,6 @@ angular.module('Geosales').controller('ClienteController', ['$scope', 'ClientesS
     navigator.geolocation.getCurrentPosition(
       function(position) {
         $scope.currentPosition = {latitude: position.coords.latitude, longitude: position.coords.longitude};
-        //$scope.cliente.navigateTo = "window.open('http://maps.google.com/?saddr=" + $scope.currentPosition.latitude + "," + $scope.currentPosition.longitude + "&daddr=" + $scope.cliente.get('location').latitude + "," + $scope.cliente.get('location').longitude + "', '_system', 'location=yes'); return false;";
         $scope.showVisitar = true;
       },
       function(error) {
@@ -183,6 +191,7 @@ angular.module('Geosales').controller('ClienteController', ['$scope', 'ClientesS
       }
     };
     $scope.$apply();
+    $scope.hideLoading();
   };
 
   var getCreditDetail = function(credit) {
